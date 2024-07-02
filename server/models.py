31bee9e768c6894a16c090db94e9869a49043b9c
BensonGitau <bensonkamaugitau@gmail.com>
2024-07-02 23:00:50 +0300

    Complete

--- server/models.py
+++ server/models.py
@@ -11,7 +11,17 @@ class Author(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators 
+    # Add validators
+    @validates('name')
+    def validate_name(self, key, name):
+        if not name or Author.query.filter_by(name=name).first():
+            raise ValueError('Author name is required')
+        return name
+    @validates('phone_number')
+    def validate_phone_number(self, key, phone_number):
+        if not phone_number.isdigit() or len(phone_number) != 10:
+            raise ValueError('Phone number is must be 10 digits and unique')
+        return phone_number
 
     def __repr__(self):
         return f'Author(id={self.id}, name={self.name})'
@@ -27,7 +37,29 @@ class Post(db.Model):
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, onupdate=db.func.now())
 
-    # Add validators  
+    # Add validators 
+    @validates('content')
+    def validate_content(self, key, content):
+        if len(content) < 250:
+            raise ValueError('Post content must be at least 250 characters long')
+        return content
+    @validates('summary')
+    def validate_summary(self, key, summary):
+        if len(summary) > 250:
+            raise ValueError('Post summary must be at least 250 characters long')
+        return summary
+    
+    @validates('category')
+    def validate_category(self, key, category):
+        if category not in ['Fiction', 'Non-Fiction']:
+            raise ValueError('Post category must be Fiction or Non-Fiction')
+        return category
+
+    @validates('title')
+    def validate_title(self, key, title):
+        if not any(phrase in title for phrase in ["Won't Believe", "Secret", "Top", "Guess"]):
+            raise ValueError('Post title is required')
+        return title
 
 
     def __repr__(self):
